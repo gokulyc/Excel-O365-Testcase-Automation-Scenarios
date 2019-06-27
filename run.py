98ca2ac5d2b7ba6d850692b93a192a14298c6dd7
Gokul Y <gokulyc@gmail.com>
2019-06-27 15:19:50 +0530

    Border and color fill not working

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import xlsxwriter
 from openpyxl.workbook import Workbook
 from openpyxl import load_workbook
 from pathlib import Path
+from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment
 
 with open('options.json') as f:
     data = json.load(f)
@@ -22,9 +23,35 @@ bd = Side(style='thick', color="000000")
     #highlight1.border = Border(left=bd, top=bd, right=bd, bottom=bd)
     # wb.add
     # wb.add_named_style(highlight1)
-ws['E9'].border = Border(left=bd, top=bd, right=bd, bottom=bd)
+#ws['C28'].border = Border(left=bd, top=bd, right=bd, bottom=bd)
+#ws['E9'].font = Font(bold=True, size=12)
+ws['C28']="=COUNTA(C8:C24)"
+ws['C28'].style='60 % - Accent6'
+i_val=ws['c28'].value
+#copy values to cells E28, G28, I28, K28 & M28
+ws['E28']=i_val
+ws['G28']=i_val
+ws['I28']=i_val
+ws['K28']=i_val
+ws['M28']=i_val
+ws['E28'].alignment =Alignment(horizontal="center")
+ws['G28'].alignment=Alignment(horizontal="center")
+ws['I28'].alignment=Alignment(horizontal="center")
+ws['K28'].alignment=Alignment(horizontal="center")
+ws['M28'].alignment=Alignment(horizontal="center")
+ws['E28'].style='60 % - Accent6'
+ws['G28'].style='60 % - Accent6'
+ws['I28'].style='60 % - Accent6'
+ws['K28'].style='60 % - Accent6'
+ws['M28'].style='60 % - Accent6'
+ws['E28'].border=Border(outline=True)
+ws['G28'].border=Border(outline=True)
+ws['I28'].border=Border(outline=True)
+ws['K28'].border=Border(outline=True)
+ws['M28'].border=Border(outline=True)
+ws['M28'].fill=PatternFill(bgColor='000000',fgColor='ffffff')
 #ws['E9'].Side(style='thick', color="000000")
-ws['E9'].font = Font(bold=True, size=12)
+
 
 
 wb.save(filename=Path(data['path1']))
